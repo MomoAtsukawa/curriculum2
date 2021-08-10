82ba04cf8a3899dac8957b33506c74220d464091
MomoAtsukawa <88420313+MomoAtsukawa@users.noreply.github.com>
2021-08-10 14:34:06 +0900

    関数課題
    関数の作成
    意味調べ

---
+++ 2-10/index.php
@@ -0,0 +1,18 @@
+<?php
+
+function getRectangular($base, $width, $height) {
+    $volume = $base * $width * $height;
+    echo "直方体の体積は".$volume."だよ";
+}
+
+getRectangular(5, 10, 8);
+
+echo "<br>";
+echo "<br>";
+echo "ハッシュ化とは"."<br>";
+echo "値をハッシュ関数に入れてハッシュ値に変換すること"."<br>";
+echo "総合テストとは"."<br>";
+echo "より実践に近い目線で行う最終テスト"."<br>";
+echo "デバックとは"."<br>";
+echo "不具合の原因を探して直すこと";
+?>
