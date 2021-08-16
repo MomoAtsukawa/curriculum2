37f5b899d944cbf06e658a5505ca8e1ea5e0073f
MomoAtsukawa <88420313+MomoAtsukawa@users.noreply.github.com>
2021-08-16 19:14:34 +0900

    ファイルのデータを読み書きする

--- 2-16/index.php
+++ 2-16/index.php
@@ -14,7 +14,7 @@
 
     $test_file = "test2.txt";
 
-    if(is_readable(test_file)) {
+    if(is_readable($test_file)) {
         $fp = fopen($test_file, "r");
         while ($line = fgets($fp)) {
             echo $line."<br>";
